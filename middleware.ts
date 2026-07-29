cbedfd544d59f858b560924a320214faabee8cb3
hejzbh <tulumovichazim18@gmail.com>
2026-07-29 15:13:30 +0200

    edit

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // MAINTENANCE MODE — postavi na false da vratiš cijeli sajt
-const MAINTENANCE_MODE = true;
+const MAINTENANCE_MODE = false;
 
 export function middleware(request: NextRequest) {
   if (!MAINTENANCE_MODE) return NextResponse.next();
